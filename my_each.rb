9629a6737cb72d51260a0d0c9659794e236e1353
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-07 18:06:00 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(array) # put argument(s) here
   i = 0
-#  while i < array.length
-#   puts array[i]
-#    i =+ 1
-#  end - this is an endless loop
+  while i < array.length
+   yield array[i]
+    i =+ 1
+  end - this is an endless loop
   return array
 end
